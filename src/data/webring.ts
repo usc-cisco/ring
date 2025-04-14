34135b8c5bf0f9fa5f3ca2d9fe81746228a25624
jcbecleo <ecleojosscharyborj@gmail.com>
2025-04-14 12:29:24 +0800

    feat: add jossecleo to webring

--- src/data/webring.ts
+++ src/data/webring.ts
@@ -23,5 +23,9 @@ export const webring: WebringEntry[] = [
     name: "Samuel Bongs",
     year: 2026,
     url: "https://sethonne.dev/",
+  },{
+    name: "Joss Ecleo",
+    year: 2025,
+    url: "https://jossecleo.dev/",
   },
 ];
